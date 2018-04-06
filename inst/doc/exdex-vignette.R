984f4447ea1e6118df1b72c43b0be3949bb269ec
Constantinos Christodoulides <constantinos.christodoulides.15@ucl.ac.uk>
2018-04-06 15:42:48 +0100

    vignette update

--- inst/doc/exdex-vignette.R
+++ inst/doc/exdex-vignette.R
@@ -1,3 +1,13 @@
+## ------------------------------------------------------------------------
+library(exdex)
+# Set a threshold at the 90% quantile
+thresh <- quantile(newlyn, probs = 0.90)
+# MLE, SE and 95% likelihood-based confidence interval
+mle <- kgaps_mle(newlyn, thresh, conf = 95)
+mle$theta_mle
+mle$theta_se
+mle$theta_ci
+
 ## ----fig.width = 5 , fig.height= 5---------------------------------------
 library(exdex)
 # Set a number for the minimum and the maximum quantile level
@@ -10,7 +20,7 @@ threshplot(newlyn, 50, 60,
   # Change the line type
            lty = c(2, 1, 2),
   # Change the colours of the lines
-           col = c(4, 5, 6),
+           col = c(1, 5, 2),
   # Change the label names for X and Y axes
            xlab = "Any name",
            ylab = "Any name",
@@ -19,4 +29,13 @@ threshplot(newlyn, 50, 60,
   # Change the symbol used
            pch = 19
            )
+
+## ------------------------------------------------------------------------
+library(exdex)
+# Decide on the block size and indicate whether to use sliding blocks or disjoint blocks 
+# MLE, SE and 95% likelihood-based confidence interval
+spm_mle(newlyn, 20, sliding = FALSE, conf = 95)
+mle$theta_mle
+mle$theta_se
+mle$theta_ci
 
